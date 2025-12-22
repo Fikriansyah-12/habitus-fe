f2284f6706b0dc4820ab04bd30a4f01eda263b67
fikri-ans <widiantoaris170@gmail.com>
2025-12-22 11:08:39 +0700

    add

--- habitus-fe/src/components/Iconify.vue
+++ habitus-fe/src/components/Iconify.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
-import Icon from "@iconify/vue";
+import { Icon } from "@iconify/vue";
+
+defineOptions({ inheritAttrs: false });
+
+type Props = {
+  icon: string; // wajib
+  width?: string | number;
+  height?: string | number;
+};
+
+const props = defineProps<Props>();
 </script>
 
 <template>
-  <Icon v-bind="$attrs" />
+  <Icon
+    :icon="props.icon"
+    :width="props.width"
+    :height="props.height"
+    v-bind="$attrs"
+  />
 </template>
